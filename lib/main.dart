e4f1c0d5adb44f7ebe50a2cc937cb393060bc501
Eyosiyas_Gezahegn <eyosiyasgezahegn326@gmail.com>
2025-12-11 21:06:37 +0000

    facebook.dart is imported

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:helloworld/splash_screen.dart';
 import 'package:helloworld/home_page.dart';
+import 'package:helloworld/facebook.dart';
 
 void main() {
   runApp(
     // const SplashScreen(),
     HomePage(),
+    //Facebook(),
   );
 }
