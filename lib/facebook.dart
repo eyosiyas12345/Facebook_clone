5fe7f7c154e73cf47f3761e73a164977d70c7102
Eyosiyas_Gezahegn <eyosiyasgezahegn326@gmail.com>
2025-12-11 21:05:59 +0000

    created for conditional rendering

---
+++ lib/facebook.dart
@@ -0,0 +1,24 @@
+// import 'package:flutter/material.dart';
+// import 'package:helloworld/HomePage.dart';
+
+// class Facebook extends StatefulWidget {
+//   const Facebook({super.key});
+
+//   @override
+//   State<Facebook> createState() {
+//     return _FacebookState();
+//   }
+// }
+
+// class _FacebookState extends State<Facebook> {
+//   @override
+//   Widget build(context) {
+//     return MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       home: Scaffold(
+//         backgroundColor: Colors.white,
+//         body: HomePage();
+//       ),
+//     );
+//   }
+// }
